7b54eb29aba7a79681b5d3e047fda31798373d06
Drew Jordan <drjorda2@gmail.com>
2016-04-30 16:36:10 -0400

    push it good

---
+++ Drew's work.R
@@ -0,0 +1,68 @@
+library(dplyr)
+library(data.table)
+library(lubridate)
+
+# Reading in the data
+pitchfx = as.data.frame(fread("Pitchfx.csv"))
+
+# Converting game_date to a date
+pitchfx = pitchfx %>%
+  mutate(game_date = dmy(game_date))
+
+# tabling the occurences of pitches
+as.data.frame(table(pitchfx$pitch_type))
+
+# Creating a new categorical variable for runners on base
+pitchfx = pitchfx %>%
+  mutate(runners = paste0( ifelse(is.na(Runneron1st_ID) == FALSE, 1, 0 ),
+                           ifelse(is.na(Runneron2nd_ID) == FALSE, 1, 0 ),
+                           ifelse(is.na(Runneron3rd_ID) == FALSE, 1, 0 )) )
+
+# Creating a new categorical variable for count
+pitchfx = pitchfx %>%
+  mutate(count = paste(pre_balls, pre_strikes, sep = "-"))
+
+# Combining low-occurence pitches into "Other Category"
+Other = c("", "AB", "FO", "IN", "PO", "SC", "UN", "EP")
+Fastballs = c("FC", "FF", "FT", "FA")
+pitchfx = pitchfx %>%
+  mutate(new_pitch_type = ifelse(pitch_type %in% Other, "Other", 
+                                 ifelse(pitch_type %in% Fastballs,"FB", pitch_type)))
+
+# tabling the occurences of pitches after pitch_type update
+as.data.frame(table(pitchfx$new_pitch_type))
+
+# Reading in data to get names from
+pitching_stats = read.csv("Pitching_Statistics.csv", header=TRUE)
+
+# Getting unique pitchers names
+pitchers = pitching_stats %>%
+  distinct(pitcher_id, Name) %>%
+  select(pitcher_id, Name)
+
+# Joining with pitchfx data to attach names
+pitchfx = left_join(pitchfx, pitchers)
+
+# Just Clayton Kershaw's pitches
+kershaw = pitchfx %>%
+  filter(Name == "Kershaw, Clayton") %>%
+  arrange(game_date, at_bat_number)
+
+# Creating a new variable for the pitch number of the game
+pitchcounts = c()
+pitchcounts[1] = 1
+for(i in 2:nrow(kershaw))
+{
+  if(kershaw$game_id[i] == kershaw$game_id[i-1])
+  {
+    pitchcounts[i] = pitchcounts[i-1] + 1
+  } else
+  {
+    pitchcounts[i] = 1
+  }
+}
+
+kershaw = kershaw %>%
+  mutate(pitch_count = pitchcounts)
+
+         
