b3114e5d36d770204c6c7a1b574e4964b613d93c
Sarah Normoyle <snormoyle@Sarahs-MacBook-Pro-4.local>
2016-05-01 16:26:04 -0400

    updating initial code

--- navie_Markov_chain.R
+++ navie_Markov_chain.R
@@ -1,16 +1,27 @@
 library(data.table)
 library(dplyr)
+library(lubridate)
 
-data = fread("Pitchfx.csv")
+data = fread("Pitchfx.csv") %>%
+  	   mutate(game_date = dmy(game_date))
+
 data = data[which(data$pitch_type != "")]
+clayton_all = dplyr::filter(data, pitcher_id == "477132")  %>%
+              dplyr::filter(pitch_type != "IN") %>%
+ 		      arrange(game_date, at_bat_number)
+clayton = clayton_all$pitch_type 
 
-# sample data
-data = data[1:1000,]
+felix_all = dplyr::filter(data, pitcher_id == "433587")  %>%
+ 		      arrange(game_date, at_bat_number)
+felix = felix_all$pitch_type 
+
+
 
 # NAIVE TRANSITION MATRIX PREDICTION 
 # create naive transition matrix to predict next pitch
 trans_matrix = function(sequence){
 	n = length(sequence)
+
 	pairs = data.frame(From = character(n-1),
 					   To = character(n-1),
 					   stringsAsFactors = FALSE)
@@ -20,15 +31,74 @@ trans_matrix = function(sequence){
 	}
 	T = table(pairs$From, pairs$To)
 	T = T / rowSums(T)
-
 	return(T)
 }
 
+init_vec = function(sequence){
+	n = length(sequence)
+	freqs = table(sequence)
+	probs = freqs / sum(freqs)
+	return(probs)
+}
 
-T = trans_matrix(data$pitch_type)
-previous = 5
-next_predction = names(which.max(T[previous,]))
+next_pitch = function(prev_pitch, T){
+	next_probs = T[prev_pitch, ]
+	pitches = colnames(T)
+	next_pitch = sample(x = pitches, size = 1, replace = TRUE, prob = next_probs)
+	#next_pitch = names(which.max(next_probs))
+	return(next_pitch)
+}
 
+
+
+
+model_game = function(T, init, true_pitches){
+	pred_pitches = c()
+	pitches = colnames(T)
+	pred_pitches[1] = sample(x = pitches, size = 1, prob = init)				  
+
+	for (i in 2:length(true_pitches)){
+		pred_pitches[i] = next_pitch(true_pitches[i-1], T)
+
+	}
+	return(pred_pitches)
+}
+
+
+true_pitches = clayton
+
+# trained model
+T = trans_matrix(clayton)
+pitches = colnames(T)
+init = init_vec(clayton)
+
+
+accuracy = c()
+for (i in 1:1){
+	predicted_pitches = model_game(T, init, true_pitches)
+	accuracy[i] = mean(predicted_pitches == true_pitches)
+
+}
+
+total_accuracy_naive = mean(accuracy)
+
+accuracy_pitch = c()
+for (i in 1:length(pitches)){
+	subset = which(true_pitches == pitches[i])
+	accuracy_pitch[i] = mean(true_pitches[subset] ==  predicted_pitches[subset])
+}
+
+
+# plot for path of pithces, predicted and true
+plot(c(1:50), factor(true_pitches[1:50]), type = "p")
+lines(c(1:50), factor(predicted_pitches[1:50]), col = "red")
+
+# plot for accuracies per pitch
+barplot(c(accuracy_pitch, total_accuracy))
+
+#SUPER NAIVE
+pred_pitches_snaive = sample(x = pitches, size = length(true_pitches), replace = TRUE, prob = init) 
+total_accuracy = mean(true_pitches == pred_pitches_snaive)
 
 # 3-D TRANSITION MATRIX PREDICTION
 # create naive but based on n-1 and n-2
--- regression.R
+++ regression.R
@@ -2,17 +2,14 @@ library(data.table)
 library(barvs)
 library(BMA)
 library(BayesLogit)
+library(dplyr)
 
-data = fread("Pitchfx.csv")
-data = data[which(data$pitch_type != "")]
-
-# sample data
-data = data[1:50000,]
-
+data = clayton_all
 # make vector of 1's and 0's for binary regression
 fast = c()
 sliders = c()
-pitches = data$pitch_type
+#pitches = data$pitch_type
+pitches = clayton
 
 for (i in 1:length(pitches)){
 	if (any(pitches[i] == (c("FA", "FF", "FT", "FC")))){
@@ -37,27 +34,26 @@ balls_table = table(factor(data$pre_balls), fast)
 
 
 
-# design matrices for variables
+# design matrices for input variables
 # var1: outs
-outs = factor(data$pre_outs)
-X.all = model.matrix(~ outs - 1)
-X1    = X.all[, -3]
+outs = factor(clayton_all$pre_outs)
+X1.all = model.matrix(~ outs - 1)
+X1    = X1.all[, -3]
 
 # var2: balls
-balls = factor(data$pre_balls)
-X.all = model.matrix(~ balls - 1)
-X2    = X.all[, -4]
+balls = factor(clayton_all$pre_balls)
+X2.all = model.matrix(~ balls - 1)
+X2    = X2.all[, -4]
 
 # var3: strikes
-strikes = factor(data$pre_strikes)
-X.all = model.matrix(~ strikes - 1)
-X3    = X.all[, -3]
+strikes = factor(clayton_all$pre_strikes)
+X3.all = model.matrix(~ strikes - 1)
+X3    = X3.all[, -3]
 
 
 # testing variable selection from BMA package
 # all data for binary regression
 data_reg = data.frame(y, X1, X2, X3)
-
 output = bic.glm(y ~ outs0 + outs1 + balls0 + balls1 + balls2 + strikes0 + strikes1,
  		glm.family="binomial", 
  		data = data_reg)
@@ -67,7 +63,7 @@ summary(output)
 # BAYESIAN BINARY LOGISTIC REGRESSION  
 
 # X design matrix: colnames are the variables
-X = cbind(X1, X2, X3)
+X = cbind(1, X1, X2, X3)
 
 # number of parameters
 P = ncol(X)
@@ -80,31 +76,36 @@ samp = 2000, burn = 500)
 post_beta = colMeans(model1$beta)
 
 # predict new probability for new_x
-new_x = c(1, 0, 0, 0, 0, 1, 0) 
+new_x = c(0, 0, 0, 0, 0, 0, 0) 
 prob = (1 + exp(-new_x %*% post_beta))^(-1)
 
 
 
 # MULTINOMIAL LOGISTIC REGRESSION
-J = length(unique(pitches))
+P = ncol(X)
+X = cbind(1, X1, X2, X3)
+J = length(unique(clayton))
 
 # design matrix for y's
-Y.all = model.matrix(~ pitches - 1)
+Y.all = model.matrix(~ clayton - 1)
 Y = Y.all[, -J]
 
 # run model
-multi_out = mlogit(Y, X, n = rep(1, length(pitches)), samp = 1000, burn = 100)
+multi_out = mlogit(Y, X, n = rep(1, length(clayton)), samp = 1000, burn = 100)
 
 betas = multi_out$beta
-post_betas = matrix(0, nrow = J-1, ncol = P)
+post_betas = matrix(0, nrow = J, ncol = P)
 
 # get posterior means and put in formula
 for (j in 1:J-1){
 	post_betas[j,] = colMeans(betas[,,j])
 }
 
+post_betas[J] = 0
+
+new_x = c(1,1,0,0,0,0,1,0)
 probs = c()
-for (j in 1:J-1){
+for (j in 1:J){
 	probs[j] = exp(new_x %*% post_betas[j,])
 }
 
@@ -112,8 +113,18 @@ for (j in 1:J-1){
 prob_vec = probs / sum(probs)
 prob_vec
 
+pitches_names = names(pitches)
+next_pitch = sample(x = pitches_names, size = 1, replace = TRUE, prob = next_probs)
 # model cross validation 
 
 # test on training, testing 
 
 # model checking
+
+
+# compare 3 things for same pitcher:
+# the regression model, the markov model
+# the naive transition matrix
+
+# compare: regresssion model for 1 pitcher vs all pitchers
+# accuracy results 
