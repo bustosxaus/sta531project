6058b89a705bba811388b10fe76b83db8405770a
Sarah Normoyle <snormoyle@wl-10-190-40-175.wireless.duke.edu>
2016-05-02 11:17:09 -0400

    added variables to model

--- regression.R
+++ regression.R
@@ -24,30 +24,11 @@ source("kershaw.R")
 #balls_table = table(factor(data$pre_balls), fast)
 
 
-
-
-# design matrices for input variables
-# var1: outs
-outs = factor(kershaw$pre_outs)
-X1.all = model.matrix(~ outs - 1)
-X1    = X1.all[, -3]
-
-# var2: balls
-balls = factor(kershaw$pre_balls)
-X2.all = model.matrix(~ balls - 1)
-X2    = X2.all[, -4]
-
-# var3: strikes
-strikes = factor(kershaw$pre_strikes)
-X3.all = model.matrix(~ strikes - 1)
-X3    = X3.all[, -3]
-
-
-
 # testing variable selection from BMA package
 # all data for binary regression
 #data_reg = data.frame(fast, X1, X2, X3)
-#output = bic.glm(fast ~ outs0 + outs1 + balls0 + balls1 + balls2 + strikes0 + strikes1,
+#output = bic.glm(fast ~ outs0 + outs1 + balls0 + 
+#  balls1 + balls2 + strikes0 + strikes1,
 # 		glm.family="binomial", 
 # 		data = data_reg)
 #summary(output)
@@ -74,46 +55,89 @@ X3    = X3.all[, -3]
 
 
 
+
 # MULTINOMIAL LOGISTIC REGRESSION
-X = cbind(1, X1, X2, X3)
-P = ncol(X)
+# design matrices for input variables
+kershaw$pre_outs = factor(kershaw$pre_outs)
+kershaw$pre_balls = factor(kershaw$pre_balls)
+kershaw$pre_strikes = factor(kershaw$pre_strikes)
+#kershaw$previous_event_type = factor(kershaw$previous_event_type)
+kershaw$previous_pitch_type[1] = "FF"
+kershaw$previous_pitch_type = factor(kershaw$previous_pitch_type)
+
+
 J = length(unique(kershaw$pitch_type))
+pitches_names = sort(unique(kershaw$pitch_type))
 
-# design matrix for y's
-Y.all = model.matrix(~ kershaw$pitch_type - 1)
+
+# design matrix for Y's
+Y.all = model.matrix(~ pitch_type - 1, data = kershaw)
 Y = Y.all[, -J]
-colnames(Y) = c("CH", "CU", "FF")
 
-# run model
-multi_out = mlogit(Y, X, n = rep(1, length(kershaw$pitch_type)), samp = 1000, burn = 100)
+# design matrix for X
+X = model.matrix(~ pre_outs + pre_balls + pre_strikes + 
+    pre_balls:pre_strikes + 
+    pitch_number + runners +
+    pitch_count + top_inning_sw + bat_side + 
+    inning + previous_pitch_type,
+    data = kershaw)
+P = ncol(X)
+
+# RUN MODEL
+multi_out = mlogit(Y, X, n = rep(1, nrow(Y)), samp = 2000, burn = 500)
+
 betas = multi_out$beta
-post_betas = matrix(0, nrow = J, ncol = P)
-
 # get posterior means and put in formula
+post_betas = matrix(0, nrow = J, ncol = P)
 for (j in 1:J-1){
 	post_betas[j,] = colMeans(betas[,,j])
 }
-
 post_betas[J] = 0
 
-new_x = c(1,1,0,0,0,0,1,0)
-probs = c()
-for (j in 1:J){
-	probs[j] = exp(new_x %*% post_betas[j,])
+
+# PREDICT PITCHES 
+# sample for testing 
+X_test = X
+
+probs = matrix(0, nrow = nrow(X_test), ncol = J)
+for (i in 1:nrow(X_test)){
+  new_x = X_test[i,]
+  for (j in 1:J){
+    probs[i,j] = exp(new_x %*% post_betas[j,])
+  }
 }
 
-# print probability vector
-prob_vec = probs / sum(probs)
-prob_vec
+# probability matrix from results of model
+prob_mat = probs / rowSums(probs)
 
-pitches_names = sort(unique(kershaw$pitch_type))
-next_pitch = sample(x = pitches_names, size = 1, replace = TRUE, prob = prob_vec)
+# find average accuracy of model
+accuracy = c()
+for (m in 1:10){
+  pred_pitches = c()
+
+  for (i in 1:nrow(prob_mat)){
+    prob_vec = prob_mat[i,]
+    pred_pitches[i] = sample(x = pitches_names, 
+        size = 1, replace = TRUE, prob = prob_vec)
+  }
+
+  accuracy[m] = mean(pred_pitches == kershaw$pitch_type)
+}
+mean(accuracy)
+
+# function to pull out variables from data frame
+# predict pitch based on model 
+
+
+new_x = c(1, rep(0, P-1))
+
+
+
+
 
 
 # MULTINOMIAL VARIABLE SELECTION
 # with package
-
-# cross validation: accuracy of pitches 
 
 ######################################### CROSS VALIDATION #########################################
 
@@ -137,35 +161,10 @@ for(i in 1:k)
   train = kershaw[-folds[[i]], ]
   
   # design matrices for input variables
-  # var1: outs
-  outs = factor(test$pre_outs)
-  X1.all = model.matrix(~ outs - 1)
-  X1    = X1.all[, -3]
   
-  # var2: balls
-  balls = factor(test$pre_balls)
-  X2.all = model.matrix(~ balls - 1)
-  X2    = X2.all[, -4]
-  
-  # var3: strikes
-  strikes = factor(test$pre_strikes)
-  X3.all = model.matrix(~ strikes - 1)
-  X3    = X3.all[, -3]
-  
-  # MULTINOMIAL LOGISTIC REGRESSION
-  X = cbind(1, X1, X2, X3)
-  P = ncol(X)
-  J = length(unique(kershaw$pitch_type))
-  
-  # design matrix for y's
-  Y.all = model.matrix(~ test$pitch_type - 1)
-  Y = Y.all[, -J]
-  colnames(Y) = c("CH", "CU", "FF")
   
   # run model
-  multi_out = mlogit(Y, X, n = rep(1, length(test$pitch_type)), samp = 1000, burn = 100)
-  betas = multi_out$beta
-  post_betas = matrix(0, nrow = J, ncol = P)
+  
   
   # get posterior means and put in formula
   for (j in 1:J-1){
