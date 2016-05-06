12aaa9075a640a8f290ee9911958bcc50cb55dc4
Sarah Normoyle <snormoyle@Sarahs-MacBook-Pro-4.local>
2016-05-05 21:12:31 -0400

    changed to frequentist regression

--- regression.R
+++ regression.R
@@ -3,10 +3,11 @@ library(BayesLogit)
 library(dplyr)
 library(mlogitBMA)
 library(caret)
+library(nnet)
 
 source("kershaw.R")
 
-
+####### FORMULA MAKER #########
 formula_maker = function(vars)
 {
   form = vars[1]
@@ -17,7 +18,7 @@ formula_maker = function(vars)
       form = paste(form, vars[t], sep = "+")
     }
   }
-  FORMULA = as.formula(paste("~", form))
+  FORMULA = as.formula(paste("~", form, "-1"))
 
   return(FORMULA)
 }
@@ -29,9 +30,9 @@ formula_maker = function(vars)
 CV = function(k, pitcher, vars){
   
   # Unique pitchers for our pitcher of interest
-  J = length(unique(pitcher$new_pitch_type))
+  J = length(unique(pitcher$pitch_type))
   # Names of pitches
-  pitches_names = sort(unique(pitcher$new_pitch_type))
+  pitches_names = sort(unique(pitcher$pitch_type))
   
   # Building our formula for model.matrix
   formula = formula_maker(vars)
@@ -46,6 +47,8 @@ CV = function(k, pitcher, vars){
   # Running k-fold CV
   holdout = c()
 
+  accuracy_matrix = matrix(0, nrow = k, ncol = J)
+
   for(i in 1:k)
   {
     # Testing set
@@ -55,28 +58,33 @@ CV = function(k, pitcher, vars){
     
     # design matrix for pitch_type
     # design matrix for Y's
-    Y.all = model.matrix(~ new_pitch_type - 1, data = train)
+    Y.all = model.matrix(~ pitch_type - 1, data = train)
     Y = Y.all[, -J]
     
     X_train = model.matrix(formula, data = train)
     P = ncol(X_train)
     
     # run model
-    multi_out = mlogit(Y, X_train, n = rep(1, nrow(Y)), samp = 1000, burn = 200)
-    betas = multi_out$beta
+    # multi_out = mlogit(Y, X_train, n = rep(1, nrow(Y)), samp = 1000, burn = 200)
+    # betas = multi_out$beta
+  
+    multi_out = multinom(train$pitch_type ~ X_train)
+    post_betas = matrix(0, nrow = J, ncol = P+1)
+    post_betas[2:J,] = coefficients(multi_out)
+    
     
     # get posterior means
-    post_betas = matrix(0, nrow = J, ncol = P)
-    for (j in 1:J-1){
-      post_betas[j,] = colMeans(betas[,,j])
-    }
-    post_betas[J] = 0
+    #post_betas = matrix(0, nrow = J, ncol = P)
+    #for (j in 1:J-1){
+    #  post_betas[j,] = colMeans(betas[,,j])
+    #}
+    #post_betas[J] = 0
     
     
     # design matrix for test set, X_test
-    X_test = model.matrix(formula,
-                            data = test)
+    X_test = model.matrix(formula, data = test)
     P = ncol(X_test)
+    X_test = cbind(1, X_test)
     
     # Finding multinomial probabilities associated with each test observation
     probs = matrix(0, nrow = nrow(X_test), ncol = J)
@@ -99,24 +107,28 @@ CV = function(k, pitcher, vars){
         prob_vec = prob_mat[g,]
         pred_pitches[g] = sample(x = pitches_names, 
                                  size = 1, replace = TRUE, prob = prob_vec)
-      } 
-      accuracy[m] = mean(pred_pitches == test$new_pitch_type)
-    }
+        }       
+      accuracy[m] = mean(pred_pitches == test$pitch_type)
+      }
     
+
+      true_pitches = test$pitch_type
+      accuracy_pitch = c()
+      for (b in 1:length(pitches_names)){
+        subset = which(true_pitches == pitches_names[b])
+          accuracy_pitch[b] = mean(true_pitches[subset] ==  pred_pitches[subset])
+      }
+
+    accuracy_matrix[i,] = accuracy_pitch
     holdout[i] = mean(accuracy)
   }
 
-
-  return(mean(holdout))
+  return(list(mean(holdout), colMeans(accuracy_matrix)))
 
 }
 
 
-
-
-# Number of folds
-
-# Variables we wish to use
+# RUN 3 DIFFERENT MODELS WITH CROSS VALIDATION 
 
 vars1 = c("count")
 vars2 = c("count", "pre_outs", "inning")
@@ -124,26 +136,52 @@ vars3 = c("pre_outs", "count", "pitch_number", "runners_count",
   "pitch_count", "top_inning_sw", "bat_side", "inning", 
   "prev_pitch_type")
 
-CV(5, kershaw, vars1)
-CV(5, kershaw, vars2)
-CV(5, kershaw, vars3)
+vars1_list = CV(5, kershaw, vars1)
+vars2_list = CV(5, kershaw, vars2)
+vars3_list = CV(5, kershaw, vars3)
+
+results = matrix(c(vars1_list[[2]], vars2_list[[2]], vars3_list[[2]]), byrow = TRUE, 
+  nrow = 3, ncol = 4)
+
+results = cbind(results, c(vars1_list[[1]], vars2_list[[1]], 
+  vars3_list[[1]]))
+
+library(xtable)
+rownames(results) = c("LR1", "LR2", "LR3")
+colnames(results) = c(pitches_names, "Total")
 
 
+############ TEST MODEL IN SAMPLE #########
+# Unique pitchers for our pitcher of interest
+J = length(unique(kershaw$pitch_type))
+
+# Names of pitches
+pitches_names = sort(unique(kershaw$pitch_type))
+  
+# Building our formula for model.matrix
+formula = formula_maker(vars3)
+
+# number of observations
+n = nrow(kershaw)
+X = model.matrix(formula, data = kershaw)
+    
+multi_out = multinom(kershaw$pitch_type ~ X)   
+pred_pitches = predict(multi_out) 
+total_accuracy = mean(kershaw$pitch_type == pred_pitches)
 
 
-
-# accuracy without each variable
-#without = matrix(0, nrow = 1, ncol = length(vars))
-#colnames(without) = vars
-
-# Iteratively removing one variable and performing CV
-#for(d in 1:length(vars))
-#{
-  # removing one variable at a time
-#  vars_remove = vars[-d]
-  # Performing CV on the model missing variable d
-#  without[1, d] = CV(k = k, pitcher = kershaw, vars = vars_remove)
+true_pitches = kershaw$pitch_type
+accuracy_pitch = c()
+for (i in 1:length(pitches_names)){
+  subset = which(pred_pitches == pitches_names[i])
+  accuracy_pitch[i] = mean(true_pitches[subset] ==  pred_pitches[subset])
 }
+
+ALL_RESULTS3 = c(accuracy_pitch, total_accuracy)
+
+
+
+###### VARIABLE SELECTION #######
 
 vars = c("pre_outs", "count", "pitch_number", "runners_count",
   "top_inning_sw", "bat_side", "inning", 
@@ -177,12 +215,3 @@ while (difference > 0){
 
   i = i + 1
 }
-
-# testing variable selection from BMA package
-# all data for binary regression
-#data_reg = data.frame(fast, X1, X2, X3)
-#output = bic.glm(fast ~ outs0 + outs1 + balls0 + 
-#  balls1 + balls2 + strikes0 + strikes1,
-#     glm.family="binomial", 
-#     data = data_reg)
-#summary(output)
